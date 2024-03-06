58237e02ae2b81da780493c5812ee393dbfdd206
Pavel Kulikov <pkulikov@secureducation.ru>
2024-03-06 11:07:53 +0700

    exclamation marks added

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
 
     public static void main(String... args) {
-        String msg = "Hello qa.guru!";
+        String msg = "Hello qa.guru!!!";
         System.out.println(msg);
     }
 }
