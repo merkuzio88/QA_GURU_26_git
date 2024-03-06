ed2663fd32c15de57e70cba6cd475e881e41f6f7
Pavel Kulikov <pkulikov@secureducation.ru>
2024-03-06 11:06:55 +0700

    change message

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
 
     public static void main(String... args) {
-        String msg = "Hello qa.guru!";
+        String msg = "Hello World!";
         System.out.println(msg);
     }
 }
